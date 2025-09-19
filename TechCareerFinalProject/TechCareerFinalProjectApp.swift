05cec423ba3453c4dbb3675b76f381461801eb90
aysenurkendirci <aysenurkendirciss@gmail.com>
2025-09-19 20:27:45 +0300

    Initial Commit

---
+++ TechCareerFinalProject/TechCareerFinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TechCareerFinalProjectApp.swift
+//  TechCareerFinalProject
+//
+//  Created by Ayşe Nur Kendirci on 19.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct TechCareerFinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
