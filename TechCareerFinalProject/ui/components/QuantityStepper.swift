e6d3b56736d2daa4b2188cd16ee02d8dfd7d8491
aysenurkendirci <aysenurkendirciss@gmail.com>
2025-09-27 14:09:26 +0300

    final commit

---
+++ TechCareerFinalProject/ui/components/QuantityStepper.swift
@@ -0,0 +1,8 @@
+//
+//  QuantityStepper.swift
+//  TechCareerFinalProject
+//
+//  Created by Ayşe Nur Kendirci on 23.09.2025.
+//
+
+import Foundation
---
+++ TechCareerFinalProject/ui/theme/Lottie.swift
@@ -0,0 +1,8 @@
+//
+//  Lottie.swift
+//  TechCareerFinalProject
+//
+//  Created by Ayşe Nur Kendirci on 27.09.2025.
+//
+
+import Foundation
